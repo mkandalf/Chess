2ed66c455644ed0a255e647b182c2849a609bdc7
Nick Meyer <nicholasmeyer@gmail.com>
2011-10-05 01:19:29 -0400

    added bitscan routine, knight attack tables, and framework for knight attack move gen

--- constants.py
+++ constants.py
@@ -1,17 +1,36 @@
 from numpy import uint64
 
+all_knight_attacks = [uint64(0)]*64
 all_king_attacks = [uint64(0)]*64
 not_a_file = uint64(0xfefefefefefefefe)
 not_h_file = uint64(0x7f7f7f7f7f7f7f7f)
 
 def init():
     init_king_attacks()
+    init_knight_attacks()
+
+def knight_attacks(knight):
+    east = east_one(knight)
+    west = west_one(knight)
+    attacks = (east|west) << 16
+    attacks |= (east|west) >> 16
+    east = east_one(east)
+    west = west_one(west)
+    attacks |= (east|west) << 8
+    attacks |= (east|west) >> 8
+    return attacks
 
 def king_attacks(king):
     attacks = east_one(king) | west_one(king)
     king |= attacks
     attacks |= (north_one(king) | south_one(king))
     return attacks
+
+def init_knight_attacks():
+    sq_BB = uint64(1)
+    for sq in xrange(64):
+        all_knight_attacks[sq] = knight_attacks(sq_BB)
+        sq_BB <<= uint64(1)
 
 def init_king_attacks():
     sq_BB = uint64(1)
--- gen.py
+++ gen.py
@@ -1,4 +1,7 @@
 import constants
+import util
+from board import KNIGHT
+from numpy import uint64
 
 def gen_moves(board):
     constants.init()
@@ -7,6 +10,7 @@ def gen_moves(board):
     # Sliding pieces - magic bitboards and hashes
 
 def gen_king_moves(board):
+    # Needs to be a list of moves
     king_index = board.king_square[board.to_move] 
     return constants.all_king_attacks[king_index] \
             & ~board.piece_BB[board.to_move] 
@@ -15,28 +19,9 @@ def gen_pawn_moves(board):
     pass
 
 def gen_knight_moves(board):
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    knights = board.piece_BB[KNIGHT] 
+    while knights:
+        loc = util.bit_scan_forward(knights)
+        knights ^= uint64(1) << loc
+        knight_moves = constants.all_knight_attacks[loc]
+        # Append moves to move list
