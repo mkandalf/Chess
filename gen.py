018cf4ab307fb65262779d964104498fcdcd5572
Nick Meyer <nicholasmeyer@gmail.com>
2011-10-06 16:53:23 -0400

    fixed push by one pawn move generation

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 import constants
 import util
-from board import KNIGHT
+from board import KNIGHT, PAWN
 from numpy import uint64
 
 def gen_moves(board):
@@ -9,6 +9,11 @@ def gen_moves(board):
     # Pawns are also relatively easy, some bit shifting
     # Sliding pieces - magic bitboards and hashes
 
+def gen_move(p_to, p_from, piece, capture, promote):
+  return uint64(((promote & 7) << 18) | ((capture & 7) << 15) \
+          | (((piece - 2) & 7) << 12) \
+          | ((p_to & 63) << 6) | (p_from & 63))
+
 def gen_king_moves(board):
     # Needs to be a list of moves
     king_index = board.king_square[board.to_move] 
@@ -16,7 +21,19 @@ def gen_king_moves(board):
             & ~board.piece_BB[board.to_move] 
 
 def gen_pawn_moves(board):
-    pass
+  move_list = []
+  froms = board.piece_BB[PAWN] & board.piece_BB[board.to_move]
+  to = (util.south_one(froms) if board.to_move else util.north_one(froms)) \
+        & board.empty_BB
+  while (to):
+    print to
+    move_list.append(
+      gen_move(util.bit_scan_forward(to), 
+               util.bit_scan_forward(
+                 to << uint64(8) if board.to_move else to >> uint64(8)),
+                PAWN, 0, 0)) 
+    to = util.clear_least_bit(to)
+  return move_list
 
 def gen_knight_moves(board):
     knights = board.piece_BB[KNIGHT] 
