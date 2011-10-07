c92624835607654a463205322783e75cd8f927c4
Nick Meyer <nicholasmeyer@gmail.com>
2011-10-07 00:10:28 -0400

    knight move list creation, added some comments and fixed indentation

--- gen.py
+++ gen.py
@@ -1,13 +1,18 @@
+# To Do: Check detection, pawn promotion, sliding pieces, captured pieces
 import constants
 import util
 from board import KNIGHT, PAWN, KING
 from numpy import uint64
 
 def gen_moves(board):
-    constants.init()
-    # King, Knight are simple
-    # Pawns are also relatively easy, some bit shifting
-    # Sliding pieces - magic bitboards and hashes
+  constants.init()
+  # King, Knight are simple
+  gen_king_moves(board)
+  gen_knight_moves(board)
+  # Pawns are also relatively easy, some bit shifting
+  gen_pawn_moves(board)
+  gen_pawn_captures(board)
+  # Sliding pieces - magic bitboards and hashes
 
 def gen_move(p_to, p_from, piece, capture, promote):
   return uint64(((promote & 7) << 18) | ((capture & 7) << 15) \
@@ -15,7 +20,6 @@ def gen_move(p_to, p_from, piece, capture, promote):
           | ((p_to & 63) << 6) | (p_from & 63))
 
 def gen_king_moves(board):
-  # Needs to be a list of moves
   move_list = []
   king_index = board.king_square[board.to_move] 
   to = constants.all_king_attacks[king_index] \
@@ -73,7 +77,7 @@ def gen_pawn_captures(board):
                 util.bit_scan_forward(
                   to << uint64(7) if board.to_move else to >> uint64(9)),
                 PAWN, 1, 0))
-    to = util.cleasr_least_bit(to)
+    to = util.clear_least_bit(to)
   to = pawn_attacks_w
   while (to):
     move_list.append(
@@ -81,13 +85,19 @@ def gen_pawn_captures(board):
                 util.bit_scan_forward(
                   to << uint64(9) if board.to_move else to >> uint64(7)),
                 PAWN, 1, 0))
-    to = util.cleasr_least_bit(to)                                       
+    to = util.clear_least_bit(to)                                       
   return move_list
 
 def gen_knight_moves(board):
-    knights = board.piece_BB[KNIGHT] 
-    while knights:
-        loc = util.bit_scan_forward(knights)
-        knights ^= uint64(1) << loc
-        knight_moves = constants.all_knight_attacks[loc]
-        # Append moves to move list
+    # Replace the 1 in the move generation with the actual capture piece
+  move_list = []
+  knights = board.piece_BB[KNIGHT] 
+  while knights:
+    loc = util.bit_scan_forward(knights)
+    knights ^= uint64(1) << loc
+    to = constants.all_knight_attacks[loc]
+    while (to):
+      move_list.append(
+        gen_move(util.bit_scan_forward(to), loc, 1, 0))
+      to = util.clear_least_bit(to)
+  return move_list
