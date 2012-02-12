fda0261b57330a57b4ecb96cd5eb5187e70235b3
Ceasar Bautista <cbautista2010@gmail.com>
2012-02-12 02:43:21 -0500

    Added tests for in_check and on_board.

--- tests.py
+++ tests.py
@@ -22,6 +22,41 @@ class PlayerTest(unittest.TestCase):
         self.assertEquals(self.player._parse_move('h7 h8', self.board), Move(None, (7, 7)))
 
 
+class BoardTest(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(set())
+
+
+class OnBoardTest(BoardTest):
+    def test_on_board_center(self):
+        self.assertTrue(self.board.on_board((4, 4)))
+
+    def test_on_board_corner1(self):
+        self.assertTrue(self.board.on_board((0, 0)))
+
+    def test_on_board_corner2(self):
+        self.assertTrue(self.board.on_board((7, 7)))
+
+    def test_on_board_corner2_2(self):
+        self.assertFalse(self.board.on_board((8, 8)))
+
+
+class InCheckTest(BoardTest):
+    def setUp(self):
+        super(InCheckTest, self).setUp()
+        self.white = Player(Color.WHITE)
+        self.black = Player(Color.BLACK)
+
+    def test_not_check(self):
+        self.board.pieces.add(King(self.white, (4, 4)))
+        self.assertFalse(self.board.in_check(self.white))
+
+    def test_in_check(self):
+        self.board.pieces.add(King(self.white, (4, 4)))
+        self.board.pieces.add(Knight(self.black, (3, 2)))
+        self.assertFalse(self.board.in_check(self.white))
+
+
 class PieceTest(unittest.TestCase):
     def setUp(self):
         self.player = Player(Color.WHITE)
