d8271f5d119a67248a3cb808a6beadb7f7a693d5
Ceasar Bautista <cbautista2010@gmail.com>
2012-02-12 02:26:01 -0500

    Cleaned up player module.

--- player.py
+++ player.py
@@ -1,4 +1,3 @@
-import random
 import re
 
 from move import Move
@@ -13,14 +12,15 @@ class Player(object):
     def __init__(self, color):
         self.color = color
 
-    _SQUARE_LOOKUP = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 
+    _SQUARE_LOOKUP = {'a': 0, 'b': 1, 'c': 2, 'd': 3,
             'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
     def _parse_square(self, square):
         """Convert a square into a tuple."""
         assert len(square) == 2, len(square)
         f, r = square
         assert f in self._SQUARE_LOOKUP, f
-        return self._SQUARE_LOOKUP[f], int(r) - 1 
+        return self._SQUARE_LOOKUP[f], int(r) - 1
 
     def _parse_move(self, string, board):
         """Try to convert a string to move.
@@ -37,19 +37,20 @@ class Player(object):
         """Request a valid move from the player."""
         while True:
             try:
-                move = self._parse_move(raw_input("Please enter your move: "), board)
+                move = self._parse_move(\
+                        raw_input("Please enter your move: "), board)
             except ValueError as e:
                 print e
             else:
                 if move.piece is None:
-                    print "No piece found at %s." % start
+                    print "No piece found."
                 elif move.piece.owner != self:
                     print "You do not own that piece."
                 elif not board.is_legal(move):
                     print "Illegal move"
                 else:
                     return move
-    
+
     def __str__(self):
         return "White" if self.color == Color.WHITE else "Black"
 
